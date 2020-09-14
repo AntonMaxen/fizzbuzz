e23d9049360a1e5461dcbef25a33feb9f7f5dfb9
Anton Maxén <Anton.maxen@gmail.com>
2020-09-14 14:13:29 +0200

    Added 42 as and option and restructured some conditions

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,19 +1,25 @@
-def fizzbuzz(start, end):
-    def evendiv(x, n): return x % n == 0
+def evendiv(x, n):
+    return x % n == 0
 
+
+def fizzbuzz(start, end):
     lines = []
     for i in range(start, end + 1):
         fizz = evendiv(i, 3)
         buzz = evendiv(i, 5)
         tot_string = ""
-        if fizz:
-            tot_string += "fizz"
 
-        if buzz:
-            tot_string += "buzz"
+        if i == 42:
+            tot_string += "Answer to the Ultimate Question of Life, the Universe, and Everything"
+        else:
+            if fizz:
+                tot_string += "fizz"
 
-        if not fizz and not buzz:
-            tot_string += str(i)
+            if buzz:
+                tot_string += "buzz"
+
+            if not fizz and not buzz:
+                tot_string += str(i)
 
         lines.append(tot_string)
 
